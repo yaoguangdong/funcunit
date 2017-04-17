26a250dbd0e339ab7f5f201d29fee85cf2dc1fe3
yaoguangdong <yaoguangdong@autohome.com.cn>
2017-04-17 17:32:07 +0800

    重构结构

--- Yaogd_web_unit/src/main/java/com/ygd/webunit/CustomBrowser.java
+++ Yaogd_web_unit/src/main/java/com/ygd/webunit/CustomBrowser.java
@@ -1,55 +1,30 @@
 package com.ygd.webunit;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.webkit.WebView;
-import android.widget.TextView;
+
+import com.yaogd.web.common.YBrowserActivity;
 
 /**
  * 专用于测试静态网页。
  * @author yaoguangdong
  * 2014-8-15
  */
-public class CustomBrowser extends Activity implements OnClickListener{
+public class CustomBrowser extends YBrowserActivity{
 
-	private WebView webView;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		setContentView(R.layout.browser_main);
-		//step 1
-		initViews();
-		//step 2
+		setContentView(initView(getLayoutInflater()));
 		//测试静态网页
-		webView.loadUrl("file:///android_asset/ygd/smartphone/index.html");
-		
+//		load("https://a4allroad.mp.allnewaudi.cn/2016v1/?smtid=484096070z1tl4zxkowz8cz0z&_ahrotate=1");
+//		load("http://2016.autobaojun.com/560/other/baojun/m_index.html");
+//		load("http://wechat.xudankeji.com/he/biyadi/phone.html");
+//		load("http://www.sgmw.com.cn/m/20170108/index.html?_ahrotate=1&ProjectID=119&MediaID=35");
+		load("https://tg.ewan.cn/dtxyjzys.jsp?lid=17949");
+//		load("https://clickc.admaster.com.cn/c/a77272,b1527253,c292,i0,m101,8a2,8b2,h");
+//		load("https://www.baidu.com/");
+
 	}
 
-	private void initViews() {
-		((TextView)findViewById(R.id.main_head_title)).setText("Inspire the Next");
-		findViewById(R.id.main_head_back).setVisibility(View.VISIBLE) ;
-		findViewById(R.id.main_head_back).setOnClickListener(this) ;
-		
-		webView = (WebView)findViewById(R.id.web_m_page_01) ;
-	}
-	
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.main_head_back:
-			if (webView.canGoBack()) {
-				webView.goBack();
-			} 
-			break;
-		default:
-			
-			break;
-		}
-		
-	}
-	
 }
